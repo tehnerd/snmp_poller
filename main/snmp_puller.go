14333135db661794e2dc38e5aa67c4ad003b9e62
tehnerd <ns.hando@gmail.com>
2014-07-03 06:16:30 +0400

    changelog:     * added logick for MAX_PULLERS (so we wont overwhelm the      system/network)     * a little bit more sophisticated cfg reader (for yet to be     implemented features and MAX_PULLERS cfg)

--- main/snmp_puller.go
+++ main/snmp_puller.go
@@ -5,6 +5,7 @@ import (
     "bufio"
     "os"
     "strings"
+    "strconv"
 )
 
 type RouterDescr struct {
@@ -14,17 +15,28 @@ type RouterDescr struct {
 }
 
 
-func ReadConfig() []RouterDescr {
+func ReadConfig() ([]RouterDescr,[]string,int) {
     var rlist []RouterDescr
+    var tasks []string
+    pullers := 10
     fd,err := os.Open(os.Args[1])
+    defer fd.Close()
     cfg_reader := bufio.NewReader(fd)
     line, err := cfg_reader.ReadString('\n')
     for err == nil{
         fields := strings.Fields(line)
-        rlist = append(rlist,RouterDescr{fields[0],fields[1],fields[2]})
+        if fields[0] == "tasks:"{
+            for cntr:=1;cntr<len(fields);cntr++{
+                tasks = append(tasks,fields[cntr])
+            }
+        } else if fields[0] == "pullers:"{
+            pullers,_ = strconv.Atoi(fields[1])
+        } else {
+            rlist = append(rlist,RouterDescr{fields[0],fields[1],fields[2]})
+        }    
         line, err = cfg_reader.ReadString('\n')
     }
-    return rlist
+    return rlist, tasks, pullers
 }
 
 func snmp_pull(RDescr RouterDescr,sync chan int){
@@ -35,12 +47,26 @@ func snmp_pull(RDescr RouterDescr,sync chan int){
 }
 
 func main(){
-    rlist := ReadConfig()
-    sync := make(chan int)
-    for cntr:=0;cntr<len(rlist);cntr++{
-        go snmp_pull(rlist[cntr],sync)
+    if len(os.Args) < 2{
+        os.Exit(1)
     }
-    for cntr:=0;cntr<len(rlist);cntr++{
+    rlist,tasks, MAX_PULLERS := ReadConfig()
+    fmt.Println(tasks)
+    fmt.Println(MAX_PULLERS)
+    sync := make(chan int)
+    running_pullers := 0
+    for cntr:=0;cntr<len(rlist);{
+        if running_pullers < MAX_PULLERS {
+            go snmp_pull(rlist[cntr],sync)
+            cntr++
+            running_pullers += 1
+        } else 
+        {
+            <- sync
+            running_pullers -= 1
+        }
+    }
+    for cntr:=0;cntr<running_pullers;cntr++{
         <- sync
     }
 }
