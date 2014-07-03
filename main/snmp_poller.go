9342d691911dbf7782e6a7c38a14ccffaabe6c37
tehnerd <ns.hando@gmail.com>
2014-07-03 06:29:41 +0400

    rename: pull -> poll ...

--- main/snmp_poller.go
+++ main/snmp_poller.go
@@ -18,7 +18,7 @@ type RouterDescr struct {
 func ReadConfig() ([]RouterDescr,[]string,int) {
     var rlist []RouterDescr
     var tasks []string
-    pullers := 10
+    pollers := 10
     fd,err := os.Open(os.Args[1])
     defer fd.Close()
     cfg_reader := bufio.NewReader(fd)
@@ -29,17 +29,17 @@ func ReadConfig() ([]RouterDescr,[]string,int) {
             for cntr:=1;cntr<len(fields);cntr++{
                 tasks = append(tasks,fields[cntr])
             }
-        } else if fields[0] == "pullers:"{
-            pullers,_ = strconv.Atoi(fields[1])
+        } else if fields[0] == "pollers:"{
+            pollers,_ = strconv.Atoi(fields[1])
         } else {
             rlist = append(rlist,RouterDescr{fields[0],fields[1],fields[2]})
         }    
         line, err = cfg_reader.ReadString('\n')
     }
-    return rlist, tasks, pullers
+    return rlist, tasks, pollers
 }
 
-func snmp_pull(RDescr RouterDescr,sync chan int){
+func snmp_poll(RDescr RouterDescr,sync chan int){
     s,_ := gosnmp.NewGoSNMP(RDescr.Name,RDescr.Community,gosnmp.Version2c,20)
     resp,_ := s.BulkWalk(30,".1.3.6.1.4.1.2011.5.25.32.4.1.4.3.3.1.")
     fmt.Println(len(resp))
@@ -50,23 +50,23 @@ func main(){
     if len(os.Args) < 2{
         os.Exit(1)
     }
-    rlist,tasks, MAX_PULLERS := ReadConfig()
+    rlist,tasks, MAX_POLLERS := ReadConfig()
     fmt.Println(tasks)
-    fmt.Println(MAX_PULLERS)
+    fmt.Println(MAX_POLLERS)
     sync := make(chan int)
-    running_pullers := 0
+    running_pollers := 0
     for cntr:=0;cntr<len(rlist);{
-        if running_pullers < MAX_PULLERS {
-            go snmp_pull(rlist[cntr],sync)
+        if running_pollers < MAX_POLLERS {
+            go snmp_poll(rlist[cntr],sync)
             cntr++
-            running_pullers += 1
+            running_pollers += 1
         } else 
         {
             <- sync
-            running_pullers -= 1
+            running_pollers -= 1
         }
     }
-    for cntr:=0;cntr<running_pullers;cntr++{
+    for cntr:=0;cntr<running_pollers;cntr++{
         <- sync
     }
 }
